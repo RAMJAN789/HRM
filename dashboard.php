a6dcfd3d8d0da83042e0c6f0364640697caf901d
RAMJAN789 <farazi789789@gmail.com>
2022-01-20 21:37:20 +0600

    first commit

---
+++ dashboard.php
@@ -0,0 +1,37 @@
+<?php 
+require_once("header.php");
+require_once("sidebar.php");?>
+<section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Dashboard</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+<section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Bordered Table</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+               <h1>Your code goes here</h1>
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+<?php require("footer.php"); ?>
